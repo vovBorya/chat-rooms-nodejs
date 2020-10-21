761bf64d6594a628bb0d1f50740db1fd50797ca8
vovBorya <vovbor02@gmail.com>
2020-10-21 23:49:46 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+import http from "http";
+import fs from "fs";
+import path from "path";
+import mime from "mime";
+
+const cache = {};
+
+const send404 = (response) => {
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write('Error 404: resource not found');
+  response.end();
+}
+
+const sendFile = (response, filePath, fileContents) => {
+  response.writeHead(
+    200,
+    {'content-type': mime.lookup(path.basename(filePath))}
+  );
+  response.end(fileContents);
+}
+
+const serveStatic = (response, cache, absPath) => {
+  if (cache[absPath]) {
+    sendFile(response, absPath, cache[absPath]);
+  } else {
+    fs.exists(absPath, (exists) => {
+      if (exists) {
+        fs.readFile(absPath, function(err, data) {
+          if (err) {
+            send404(response);
+          } else {
+            cache[absPath] = data;
+            sendFile(response, absPath, data);
+          }
+        });
+      } else {
+        send404(response);
+      }
+    });
+  }
+}
+
+const server = http.createServer((req, res) => {
+  const filePath = (req.url === '/') ? 'public/index.html': 'public' + req.url;
+  const absPath = './' + filePath;
+  serveStatic(res, cache, absPath)
+});
+
+server.listen(3000, () => {
+  console.log("Server listening on port 3000");
+})

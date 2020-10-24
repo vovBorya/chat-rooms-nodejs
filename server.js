e025688eb257beeed638f44b596b21f7674a2f71
vovBorya <vovbor02@gmail.com>
2020-10-24 17:32:38 +0300

    added socket handlers

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import http from "http";
-import fs from "fs";
-import path from "path";
-import mime from "mime";
-import chatServer from "./lib/chat-server";
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
+import mime from 'mime';
+import chatServer from './lib/chat-server';
 
 const cache = {};
 
@@ -10,15 +10,15 @@ const send404 = (response) => {
   response.writeHead(404, {'Content-Type': 'text/plain'});
   response.write('Error 404: resource not found');
   response.end();
-}
+};
 
 const sendFile = (response, filePath, fileContents) => {
   response.writeHead(
-    200,
-    {'content-type': mime.lookup(path.basename(filePath))}
+      200,
+      {'content-type': mime.lookup(path.basename(filePath))},
   );
   response.end(fileContents);
-}
+};
 
 const serveStatic = (response, cache, absPath) => {
   if (cache[absPath]) {
@@ -39,16 +39,16 @@ const serveStatic = (response, cache, absPath) => {
       }
     });
   }
-}
+};
 
 const server = http.createServer((req, res) => {
   const filePath = (req.url === '/') ? 'public/index.html': 'public' + req.url;
   const absPath = './' + filePath;
-  serveStatic(res, cache, absPath)
+  serveStatic(res, cache, absPath);
 });
 
 server.listen(3000, () => {
-  console.log("Server listening on port 3000");
+  console.log('Server listening on port 3000');
 });
 
 chatServer.listen(server);
